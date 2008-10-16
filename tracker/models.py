b70660612d26b8ca0f119b17427373c0dcc95f77
Mikkel Hoegh <mikkel@hoegh.org>
2008-10-17 00:48:07 +0200

    Adding a simple model.

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Slip(models.Model):
+    name = models.CharField(max_length=128)
+    #project = models.ForeignKey(Project)
+    #client = models.ForeignKey(Client)
+    #type = models.CharField(max_length=32)
+    due = models.DateField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created"]
+
+class TimeSlice(models.Model):
+    begin = models.DateTimeField(auto_now_add=True)
+    end = models.DateTimeField(null=True, blank=True)
+    slip = models.ForeignKey(Slip)
+
+    class Meta:
+        ordering = ["-begin"]
