45eb868c5c0a6f3d09b5ce045bb0892ea51be6b6
Mikkel Hoegh <mikkel@hoegh.org>
2008-10-20 13:51:19 +0200

    Some more model work, method for updating duration.

--- tracker/models.py
+++ tracker/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import datetime
 
 class Slip(models.Model):
     name = models.CharField(max_length=128)
     #project = models.ForeignKey(Project)
     #client = models.ForeignKey(Client)
     #type = models.CharField(max_length=32)
-    due = models.DateField()
+    due = models.DateField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -19,6 +20,28 @@ class TimeSlice(models.Model):
     begin = models.DateTimeField(auto_now_add=True)
     end = models.DateTimeField(null=True, blank=True)
     slip = models.ForeignKey(Slip)
+    duration = models.PositiveIntegerField(editable=False, default=0)
+
+    def __unicode__(self):
+        if self.duration == 0:
+            self.update_duration()
+
+        if self.duration:
+            delta = datetime.timedelta(seconds=self.duration)
+            return '%i days, %i seconds' % (delta.days, delta.seconds)
+        if self.end:
+            return 'From %s to %s' % (self.begin, self.end)
+        else:
+            return 'From %s' % self.begin
+
+    def update_duration(self):
+        if self.end:
+            time = self.end - self.begin
+            self.duration = time.days * 86400 + time.seconds
+            self.save()
+        else:
+            self.duration = 0
+            self.save()
 
     class Meta:
         ordering = ["-begin"]
