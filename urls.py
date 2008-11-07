c2a0e90e39f2cc69428673a818d37d8c458537ef
Mikkel Hoegh <mikkel@hoegh.org>
2008-11-07 20:00:52 +0100

    Renaming URLs

--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^djime/', include('djime.tracker.urls')),
+    (r'^tracker/', include('djime.tracker.urls')),
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
