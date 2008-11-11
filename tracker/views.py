e473cb135ac8d0473ab4dff574977c4898a33f4b
Mikkel Hoegh <mikkel@hoegh.org>
2008-11-11 23:39:56 +0100

    Working on templates, added jQuery and set up basic templates, mostly based on Pinax. Changed views to use new templates.

--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,24 @@
-from django.http import HttpResponse, HttpResponseNotAllowed
+from django.http import *
 from django.shortcuts import render_to_response, get_object_or_404
+from djime.tracker.models import Slip, TimeSlice
 
 def index(request):
-    return HttpResponseNotAllowed()
+    return render_to_response('tracker/index.html', {})
 
 def slip(request, slip_id):
-    return HttpResponseNotAllowed(('GET', 'PUT', 'DELETE'))
+    valid_methods = ('GET', 'PUT', 'DELETE')
+
+    if request.method not in valid_methods:
+        return HttpResponseNotAllowed(('GET', 'PUT', 'DELETE'))
+    else:
+        slip = get_object_or_404(Slip, pk=slip_id)
+
+        if request.method == 'GET':
+            return render_to_response('tracker/slip.html', {'slip': slip})
+
+
+
+
 
 def slip_action(request, slip_id, action):
     return HttpResponseNotAllowed(('PUT',))
