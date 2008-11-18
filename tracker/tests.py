ab8bbb33d9edb80b60eb5f7dd55f816cb0b5264e
Jakob T. Svendsen <jakob@revealit.dk>
2008-11-18 12:52:37 +0100

    Add Unittest to test Slip creation and name change function plus start and stop actions.

--- tracker/tests.py
+++ tracker/tests.py
@@ -1,9 +1,10 @@
 import unittest
 import urllib
-from django.test.client import Client, BOUNDARY, MULTIPART_CONTENT
+from django.test.client import *
 from django.contrib.auth.models import User
 from django.utils.http import urlencode
 from djime.tracker.models import Slip, TimeSlice
+from datetime import datetime
 
 
 class RESTClient(Client):
@@ -92,7 +93,28 @@ class SlipRESTActionsTestCase(unittest.TestCase):
         self.fish = Slip(name='Prepare for fishing trip', user=self.john)
         self.fish.save()
 
+        # Set up a test user
+        self.johny = User(username='johny')
+        self.johny.set_password('ponies')
+        self.johny.save()
+
+        # And a slip owned by our user.
+        self.fish = Slip(name='Prepare for fishing trip', user=self.john)
+        self.fish.save()
+
+         # And a slip owned by our user.
+        self.fishy = Slip(name='Prepare for fishing trip', user=self.johny)
+        self.fishy.save()
+
         self.client = RESTClient()
+
+
+    def tearDown(self):
+        self.john.delete()
+        self.johny.delete()
+        self.fish.delete()
+        self.fishy.delete()
+
 
     def testGet(self):
         # Before we log in, we should be redirected to the login page.
@@ -117,3 +139,95 @@ class SlipRESTActionsTestCase(unittest.TestCase):
         # it has been deleted.
         response = self.client.get('/tracker/slip/%i/' % self.fish.pk)
         self.failUnlessEqual(response.status_code, 404)
+
+
+    def testChangeSlipName(self):
+        # First login
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'john', 'password': 'ponies'})
+
+        # Then getting the slip
+        response = self.client.get('/tracker/slip/%i/' % self.fish.pk)
+
+        # Now let's change the slip name
+        response = self.client.put('/tracker/slip/%i/' % self.fish.pk,
+                                   {'name': 'Polishing horses'})
+        self.failUnlessEqual(response.status_code, 200)
+
+        # Lets reload the object from the db
+        response = self.client.get('/tracker/slip/%i/' % self.fish.pk)
+        self.failUnlessEqual(response.status_code, 200)
+
+        # Last, we're going to check the new name of the slip
+        self.failUnlessEqual(self.fish.name, 'Polishing horses')
+
+
+    def testStartTimeSlice(self):
+        # Login
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'john', 'password': 'ponies'})
+
+        # Get slip
+        response = self.client.get('/tracker/slip/%i/' % self.fish.pk)
+
+        # Send the request to start a new timeslip
+        # first we'll create a time to send, that we can use to compare later
+        timeSetBegin = datetime.now()
+        response = self.client.put('/tracker/slip/%i/start/' % self.fish.pk,
+                                   {'begin': timeSetBegin})
+        self.failUnlessEqual(response.status_code, 200)
+
+        # Let's see if the timeslice has been created and have the correct start time
+        try:
+            timeSlice = TimeSlice.objects.get(begin = timeSetBegin)
+
+        except TimeSlice.DoesNotExist:
+            self.fail('Failed to get TimeSlice, TimeSlice has not been created with correct begin time')
+
+
+    def testStopTimeSlice(self):
+        # Login
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'john', 'password': 'ponies'})
+
+        # Get slip
+        response = self.client.get('/tracker/slip/%i/' % self.fish.pk)
+
+        # Send the request to start a new timeslip
+        # first we'll create a time to send, that we can use to compare later
+        timeSetBegin = datetime.now()
+        response = self.client.put('/tracker/slip/%i/start/' % self.fish.pk,
+                                   {'begin': timeSetBegin})
+
+        # Now stopping the timeslip with a new timeSet
+        timeSetEnd = datetime.now()
+        response = self.client.put('/tracker/slip/%i/stop/' % self.fish.pk,
+                                   {'end': timeSetEnd})
+        self.failUnlessEqual(response.status_code, 200)
+
+        # Lets see if we have a created timeslice with the correct end time
+        # we do that, by try, exepting that is does not exist.
+        try:
+            timeSlice = TimeSlice.objects.get(end = timeSetBegin)
+
+        except TimeSlice.DoesNotExist:
+            self.fail('Failed to get TimeSlice, TimeSlice has not been stopped with correct end time')
+
+    def testCreateSlip(self):
+         # Login
+        response = self.client.post('/accounts/login/',
+                                    {'username': 'john', 'password': 'ponies'})
+
+        # Get slip
+        response = self.client.get('/tracker/slip/%i/' % self.fish.pk)
+
+        # Create the slip
+        response = self.client.post('/tracker/slip/create/',
+                                    {'name': 'Working all night with 5 ponies'})
+
+        # now lets see if our slip has been created
+        try:
+            Slip.objects.get(name = 'Working all night with 5 ponies')
+
+        except Slip.DoesNotExist:
+            self.fail('Failed to get Slip, Slip has not been created with the correct time')
