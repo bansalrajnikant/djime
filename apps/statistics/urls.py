86e66fa39d21f90cacf1718e0208408e17a8a874
Jakob T. Svendsen <jakob@revealit.dk>
2008-12-01 12:37:47 +0100

    Created a view to display a chart for chosen dates.

--- apps/statistics/urls.py
+++ apps/statistics/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('statistics.views',
     (r'^user/(?P<user_id>\d+)/year/(?P<year>\d{4,4})/month/(?P<month>\d{1,2})/$', 'month'),
     (r'^user/(?P<user_id>\d+)/week/$', 'todays_week'),
     (r'^user/(?P<user_id>\d+)/month/$', 'todays_month'),
+    (r'^user/(?P<user_id>\d+)/date/$', 'date'),
+    (r'^data/user/(?P<user_id>\d+)/date/$', 'get_date_data', {'start_date': [2008, 11,14], 'end_date' : [2008, 12, 01]}),
     (r'^data/user/(?P<user_id>\d+)/(?P<action>(week|month))/(?P<data>\d{1,2})/year/(?P<year>\d{4,4})/$', 'get_data'),
 )
--- apps/statistics/views.py
+++ apps/statistics/views.py
@@ -1,6 +1,6 @@
 from django.http import *
 from django.db import models
-from datetime import datetime
+import datetime
 from tracker.models import Slip, TimeSlice
 from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, get_object_or_404
@@ -13,8 +13,8 @@ def todays_week(request, user_id):
     if int(request.user.id) != int(user_id):
         return HttpResponseForbidden('Access denied')
     else:
-        week = datetime.now().isocalendar()[1]
-        year = datetime.now().isocalendar()[0]
+        week = datetime.datetime.now().isocalendar()[1]
+        year = datetime.datetime.now().isocalendar()[0]
         return render_to_response('statistics/week.html', {'week' : week, 'year': year, 'user_id': user_id},
                                       context_instance=RequestContext(request))
 
@@ -34,8 +34,8 @@ def todays_month(request, user_id):
     if int(request.user.id) != int(user_id):
         return HttpResponseForbidden('Access denied')
 
-    month = datetime.now().month
-    year = datetime.now().year
+    month = datetime.datetime.now().month
+    year = datetime.datetime.now().year
     return render_to_response('statistics/month.html', {'month' : month, 'year': year, 'user_id': user_id},
                                       context_instance=RequestContext(request))
 
@@ -46,6 +46,14 @@ def month(request, user_id, year, month):
         return HttpResponseForbidden('Access denied')
 
     return render_to_response('statistics/month.html', {'month' : month, 'year': year, 'user_id': user_id},
+                                      context_instance=RequestContext(request))
+
+@login_required()
+def date(request, user_id):
+    if int(request.user.id) != int(user_id):
+        return HttpResponseForbidden('Access denied')
+
+    return render_to_response('statistics/date.html', {'user_id': user_id},
                                       context_instance=RequestContext(request))
 
 
@@ -638,4 +646,68 @@ def get_data(request, action, data, year, user_id):
                             '"tip": "#y_label# X label [#x_label#], Value [#val#] Total [#total#]" } ],'
                             '"title": { "text": "' + str(year)+ ' ' + month_text + '" , "style": "{font-size: 20px; color: #F24062; text-align: center;}" },'
                             '"x_axis": { "labels": { "labels": [ ' + label + '] } },'
+                            '"y_axis": {  "min": 0, "max": '+str(max_val)+', "steps": '+str(step)+' }, "tooltip": { "mouse": 2 } }')
+
+
+def get_date_data(request, user_id, start_date, end_date):
+    # we want start and end date to be lists: [yyyy, mm, dd]:
+
+    s_date = datetime.date(start_date[0], start_date[1], start_date[2])
+    e_date = datetime.date(end_date[0], end_date[1], end_date[2])
+    w_date = s_date
+    dates = {}
+    while w_date != e_date:
+        dates[w_date]=[]
+        w_date += datetime.timedelta(days=1)
+    dates[e_date] = []
+
+    slice_set = TimeSlice.objects.filter(user = user_id, create_date__gte=s_date, create_date__lte=e_date)
+    for slice in slice_set:
+        if slice.slip not in dates[slice.create_date]:
+            dates[slice.create_date].append(slice.slip)
+
+    max_list = [0.01]
+    for date in dates.keys():
+        i=0
+        temp = ''
+        temp_max = 0.0
+        while i < len(dates[date]):
+            if not temp:
+                temp += '['
+            temp += '{ "val" :' + dates[date][i].display_days_time(date) + ', "tip": "' + dates[date][i].name + '<br> time: #val# total: #total#"}'
+            temp_max += float(dates[date][i].display_days_time(date))
+            if i != len(dates[date])-1:
+                temp += ','
+            if i == len(dates[date])-1:
+                temp += ']'
+            i += 1
+        if not temp:
+            temp = '[0]'
+        dates[date] = temp
+        max_list.append(temp_max)
+
+    max_val = max(max_list)
+    step = max_val*0.1
+
+    label_list = []
+
+    for key in dates.keys():
+        label_list.append(key)
+
+    label_list.sort()
+
+    val_all = ''
+    for sorted_date in label_list:
+        val_all += dates[sorted_date] + ','
+
+    label = ''
+    for labl in label_list:
+        label += '"' + str(labl.day) +'",'
+
+    return HttpResponse('{ "elements": [ { "type": "bar_stack",'
+                            '"colours": [ "#F00000", "#FF0000", "#FFF000", "#FFFF00", "#FFFFF0", "#FFFFFF" ],'
+                            '"values": ['+ val_all[:-1] +'],'
+                            '"tip": "#y_label# X label [#x_label#], Value [#val#] Total [#total#]" } ],'
+                            '"title": { "text": "From ' + str(start_date)+ ' to ' + str(end_date) + '" , "style": "{font-size: 20px; color: #F24062; text-align: center;}" },'
+                            '"x_axis": { "labels": { "labels": [ ' + label[:-1] + '] } },'
                             '"y_axis": {  "min": 0, "max": '+str(max_val)+', "steps": '+str(step)+' }, "tooltip": { "mouse": 2 } }')
