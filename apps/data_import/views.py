0aa9c60bcf23a57b835cc0fd9815665a12b62d7e
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-08 22:49:31 +0100

    Access denied on mismatch between import owner and user.

--- apps/data_import/views.py
+++ apps/data_import/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseForbidden, HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.core.urlresolvers import reverse
@@ -22,29 +22,31 @@ def import_form(request):
                               context_instance=RequestContext(request))
 @login_required
 def confirm(request, import_id, action):
-    if request.user.id == Import.objects.get(pk=import_id).user_id:
-        if request.method == 'GET':
-            if action == 'confirm':
-                data = depickle_preview(import_id)
-                return render_to_response('data_import/confirm.html', {'import_data': data['import_data'], 'import_id': import_id},
-                                                context_instance=RequestContext(request))
+    if request.user.id != Import.objects.get(pk=import_id).user_id:
+        return HttpResponseForbidden('Access denied')
 
-        if request.method == 'POST':
-            if action == 'save':
-                result = depickle_import(import_id, request.user.id)
-                action = 'save'
+    if request.method == 'GET':
+        if action == 'confirm':
+            data = depickle_preview(import_id)
+            return render_to_response('data_import/confirm.html', {'import_data': data['import_data'], 'import_id': import_id},
+                                            context_instance=RequestContext(request))
 
-            elif action == 'cancel':
-                result = delete_pickles(import_id, request.user.id)
-                action = 'cancel'
+    if request.method == 'POST':
+        if action == 'save':
+            result = depickle_import(import_id, request.user.id)
+            action = 'save'
 
-            else:
-                return HttpResponse('Invalid post action')
+        elif action == 'cancel':
+            result = delete_pickles(import_id, request.user.id)
+            action = 'cancel'
 
-            if result == 'succes':
-                return HttpResponseRedirect(reverse('data_import_results', args=(import_id, action)))
-            else:
-                return HttpResponse(result)
+        else:
+            return HttpResponse('Invalid post action')
+
+        if result == 'succes':
+            return HttpResponseRedirect(reverse('data_import_results', args=(import_id, action)))
+        else:
+            return HttpResponse(result)
 
 
     else:
@@ -54,6 +56,6 @@ def results(request, import_id, action):
     if action == 'save':
         result = 'Your data have been saved'
     elif action == 'cancel':
-        result = 'Your data have not been save'
+        result = 'Your data have not been saved'
     return render_to_response('data_import/results.html', {'result': result},
                               context_instance=RequestContext(request))
