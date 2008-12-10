19b072205ac90a3e1a67ca7dfba73b795f32a4c7
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-10 22:54:07 +0100

    Removed redundant ForeignKey parameters (both are false per default) Added created timestamp.

--- apps/data_import/models.py
+++ apps/data_import/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Import(models.Model):
+    user = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
     complete_data = models.FileField(upload_to='import_data/complete/%Y/%m/')
     partial_data = models.FileField(upload_to='import_data/partial/%Y/%m/')
-    user = models.ForeignKey(User, blank=False, null=False)
