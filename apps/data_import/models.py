53214bbb57fd131f4e5fa59642806ecf2b1767ee
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-10 22:56:01 +0100

    Added a completion timestamp as well.

--- apps/data_import/models.py
+++ apps/data_import/models.py
@@ -4,5 +4,6 @@ from django.contrib.auth.models import User
 class Import(models.Model):
     user = models.ForeignKey(User)
     created = models.DateTimeField(auto_now_add=True)
+    completed = models.DateTimeField(blank=True, null=True)
     complete_data = models.FileField(upload_to='import_data/complete/%Y/%m/')
     partial_data = models.FileField(upload_to='import_data/partial/%Y/%m/')
