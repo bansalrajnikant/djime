45a5cf81fc21307cc01d190f03454201e39f18e2
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-27 12:28:01 +0100

    Reformatted and commented message code.

--- static/js/djime.js
+++ static/js/djime.js
@@ -1,11 +1,14 @@
 /* JavaScript common to the entire Djime app. */
 
 $(document).ready(function () {
-    // Add rounded corners and a "close" button.
+    // Message sendt from django auth messaging.
     $("#content #messages li")
+    // Add a close button
     .prepend('<span class="close">&times;</span>')
-    .corner();
-    $("#content #messages li").click(function () {
+    // Rounded corners
+    .corner()
+    // And hide it on click
+    .click(function () {
         $(this).fadeOut()
     });
 });
