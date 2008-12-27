2d456d7eaf234199fbc39ed728de795666c4390d
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-27 12:18:10 +0100

    Close "button" on messages.

--- static/js/djime.js
+++ static/js/djime.js
@@ -1,9 +1,12 @@
 /* JavaScript common to the entire Djime app. */
 
 $(document).ready(function () {
+    // Add rounded corners and a "close" button.
+    $("#content #messages li")
+    .prepend('<span class="close">&times;</span>')
+    .corner();
     $("#content #messages li").click(function () {
         $(this).fadeOut()
     });
-    $("#content #messages li").corner();
 });
 
