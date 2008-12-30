d756b49d524c3b474059079df687cac5f3f57845
Jakob T. Svendsen <jakob@revealit.dk>
2008-12-30 11:58:56 +0100

    Added Clients view, and added more time calcs to project view.

--- apps/project/views.py
+++ apps/project/views.py
@@ -42,5 +42,43 @@ def show_project(request, project_id):
             seconds += slice.duration
         duration += seconds
     time ='%02i:%02i' % (duration/3600, duration%3600/60)
-    return render_to_response('project/project.html', {'project': project, 'slip_user': slip_set_user, 'slip_rest': slip_set_exclude_user, 'slip_all': slip_set_all, 'time': time},
+    duration = 0
+    for slip in slip_set_user:
+        seconds = 0
+        for slice in slip.timeslice_set.all():
+            seconds += slice.duration
+        duration += seconds
+    time_user ='%02i:%02i' % (duration/3600, duration%3600/60)
+    duration = 0
+    for slip in slip_set_user:
+        seconds = 0
+        for slice in slip.timeslice_set.all():
+            seconds += slice.duration
+        duration += seconds
+    time_other ='%02i:%02i' % (duration/3600, duration%3600/60) 
+    return render_to_response('project/project.html', {'project': project, 'slip_user': slip_set_user, 'slip_rest': slip_set_exclude_user, 'slip_all': slip_set_all, 'time': time, 'time_user': time_user, 'time_other': time_other},
+                                      context_instance=RequestContext(request))
+
+
+def show_all_clients(request):
+    clients = Client.objects.all()
+    return render_to_response('project/all_clients.html', {'clients': clients},
+                                        context_instance=RequestContext(request))
+
+
+def show_client(request, client_id):
+    client = get_object_or_404(Client, pk=client_id)
+    #if request.user not in client.members.all():
+        #return HttpResponseForbidden('Access Denied')
+    slip_set_all = Slip.objects.filter(client=client)
+    slip_set_user = slip_set_all.filter(user=request.user)
+    slip_set_exclude_user = slip_set_all.exclude(user=request.user)
+    duration = 0
+    for slip in slip_set_all:
+        seconds = 0
+        for slice in slip.timeslice_set.all():
+            seconds += slice.duration
+        duration += seconds
+    time ='%02i:%02i' % (duration/3600, duration%3600/60)
+    return render_to_response('project/client.html', {'client': client, 'slip_user': slip_set_user, 'slip_rest': slip_set_exclude_user, 'slip_all': slip_set_all, 'time': time},
                                       context_instance=RequestContext(request))
