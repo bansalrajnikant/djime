4da9691f3c6723d8d5e05b784b69cdb9b9fdf333
Mikkel Hoegh <mikkel@hoegh.org>
2008-12-31 14:20:03 +0100

    Fixed syntax error.

--- apps/project/client_urls.py
+++ apps/project/client_urls.py
@@ -4,6 +4,6 @@ urlpatterns = patterns('project.views',
     url(r'^$', 'client_index', name='client_index'),
     url(r'^(?P<client_id>\d+)/$', 'show_client', name='client_page'),
     url(r'^(?P<user_type>(user|team|project))/(?P<user_id>\d+)/clients/$',
-        'show_user_clients'name='user_client_list'),
+        'show_user_clients', name='user_client_list'),
 )
 
