05f3f83ebac4177605548e1f8d1b09336f60d633
Mikkel Hoegh <mikkel@hoegh.org>
2009-01-05 21:54:39 +0100

    Renamed and moved DataImport model to the main Djime models.

--- djime/models.py
+++ djime/models.py
@@ -89,3 +89,10 @@ class TimeSlice(models.Model):
     class Meta:
         ordering = ["-begin"]
 
+class DataImport(models.Model):
+    user = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    completed = models.DateTimeField(blank=True, null=True)
+    complete_data = models.FileField(upload_to='import_data/complete/%Y/%m/')
+    partial_data = models.FileField(upload_to='import_data/partial/%Y/%m/')
+
