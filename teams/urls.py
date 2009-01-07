c1402c6f5c0b941b6cd79ce73df33a83b34b8054
Jakob T. Svendsen <jakob@revealit.dk>
2009-01-07 11:37:57 +0100

    Redone urls for teams.

--- teams/urls.py
+++ teams/urls.py
@@ -4,8 +4,8 @@ from teams.models import Team
 
 urlpatterns = patterns('teams.views',
         url(r'^create/$', 'create', name="team_create"),
-        url(r'^your_teams/$', 'your_teams', name="your_teams"),
-        url(r'^team/(\w+)/$', 'team', name="team_detail"),
-        url(r'^team/(?P<slug>\w+)/delete/$', 'delete', name="team_delete"),
+        url(r'^$', 'index', name="team_index"),
+        url(r'^(?P<slug>\w+)/$', 'team', name="team_display"),
+        url(r'^(?P<slug>\w+)/delete/$', 'delete', name="team_delete"),
     )
 
