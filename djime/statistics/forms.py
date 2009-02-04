7c091548939422a6033e9e53d01107f61920679a
Jakob Torp Svendsen <jakob@revealit.dk>
2009-02-04 23:41:54 +0100

    bug fix, error msg need to be translated by ugettext

--- djime/statistics/forms.py
+++ djime/statistics/forms.py
@@ -2,6 +2,7 @@ from django import forms
 import datetime
 import re
 from exceptions import ValueError
+from django.utils.translation import ugettext as trans
 
 class DateSelectionForm(forms.Form):
     date = forms.CharField()
@@ -12,19 +13,19 @@ class DateSelectionForm(forms.Form):
         if date_data:
             date_list = date_data.split()
             if len(date_list) != 3:
-                raise forms.ValidationError(_("You have to enter 2 dates, in the date field, seperated by SPACE. Hint, use the date picker provided"))
+                raise forms.ValidationError(trans("You have to enter 2 dates, in the date field, seperated by SPACE. Hint, use the date picker provided"))
             else:
                 start_date = date_list[0]
                 end_date = date_list[-1]
                 if not re.match("[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$", start_date):
-                    raise forms.ValidationError(_("Start date has invalid format, must be 'yyyy-mm-dd'"))
+                    raise forms.ValidationError(trans("Start date has invalid format, must be 'yyyy-mm-dd'"))
 
                 if not re.match("[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$", end_date):
-                    raise forms.ValidationError(_("Start date has invalid format, must be 'yyyy-mm-dd'"))
+                    raise forms.ValidationError(trans("Start date has invalid format, must be 'yyyy-mm-dd'"))
 
                 if not re.match("[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$", start_date) or not re.match("[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$", end_date):
                     if start_date == end_date:
-                        raise forms.ValidationError(_("You have to choose 2 different days"))
+                        raise forms.ValidationError(trans("You have to choose 2 different days"))
 
                 start = start_date.split('-')
                 end = end_date.split('-')
@@ -33,19 +34,19 @@ class DateSelectionForm(forms.Form):
                     a= 0
                 except ValueError:
                     a=1
-                    raise forms.ValidationError(_("Start date does not exist"))
+                    raise forms.ValidationError(trans("Start date does not exist"))
                 try:
                     e_date = datetime.date(int(end[0]),int(end[1]),int(end[2]))
                     b = 0
                 except ValueError:
-                    raise forms.ValidationError(_("End date does not exist"))
+                    raise forms.ValidationError(trans("End date does not exist"))
                     b=1
 
                 if a == 0 and b == 0:
-                    if e_date.__sub__(s_date) > datetime.timedelta(days=60):
-                        raise forms.ValidationError(_("Difference between end and start date must be lower than 60 days"))
-                    if e_date.__sub__(s_date) < datetime.timedelta(days=1):
-                        raise forms.ValidationError(_("End date must be after start date"))
+                    if e_date - (s_date) > datetime.timedelta(days=60):
+                        raise forms.ValidationError(trans("Difference between end and start date must be lower than 60 days"))
+                    if e_date - (s_date) < datetime.timedelta(days=1):
+                        raise forms.ValidationError(trans("End date must be after start date"))
 
                     cleaned_data['start'] = start_date
                     cleaned_data['end'] = end_date
