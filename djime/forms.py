1fec37c64951af9b0d73e30e4898ba91720f0dac
Mikkel Hoegh <mikkel@hoegh.org>
2009-02-07 17:00:09 +0100

    Simplified validation code.

--- djime/forms.py
+++ djime/forms.py
@@ -1,57 +1,26 @@
 from django import forms
-from djime.models import Slip
 from django.db import models
-from project.models import Project
 from django.utils.translation import ugettext as _
+from djime.models import Slip
+from project.models import Project
 
 
 class SlipAddForm(forms.ModelForm):
-    project = forms.ModelChoiceField(Project.objects.all(), required=False, widget=forms.widgets.TextInput)
-    
-    def __init__(self, user, *args, **kwargs):
-            super(SlipAddForm, self).__init__(*args, **kwargs)
-            self.fields['project'].queryset = Project.objects.filter(members=user)
-    
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        data = self.data
-        error = self._errors
-        # not_a_member: a value to track if the user is member of project(s) found
-        not_a_member = True 
-        # Check to see if input contains something => 
-        # project field is filled in the form
-        if data.has_key('input'):
-            # get projects by a non case sensetive seach on the project name
-            projects = Project.objects.filter(name__iexact=data['input'])
-            # if no project could be found send error message and set
-            # not a member to False.
-            if not projects:
-                error['project'] = [_(u'Project does not exist')]
-                data['project'] = data['input']
-                not_a_member = False
-            else:
-                # If we do get something back on our search, it will be in a
-                # list. There can be more than one result. Iterate to see if
-                # the user is member of any of the projects. We are satisfied
-                # if we find one project that the user is member of. We wont
-                # Support if the user is member of more than one project of the
-                # same name, and try to figure out which project the alip should
-                # join. In this case the slip will be placed under the last
-                # project that matches.
-                for project in projects:
-                    if data['user'] in project.members.all():
-                        cleaned_data['project'] = project.name
-                        data['project'] = project.name
-                        not_a_member = False
+    project = forms.CharField(required=False)
 
-                # if projects was found but the user was not a member in any of them
-                # not a member will be true and error message is sent.
-                if not_a_member:
-                    error['project'] = [_('You are not a member of this project.')]
-                    data['project'] = project.name
-                
-        return cleaned_data
-    
+    def clean_project(self):
+        """
+        Cleaning/validation method for the project field
+        """
+        cleaned_data = self.cleaned_data
+        if cleaned_data.has_key('project'):
+            project = Project.objects.filter(name__iexact=cleaned_data['project']).filter(members=self.data['user'])[:1]
+            if project:
+                return project[0]
+            else:
+                raise forms.ValidationError(_('%s is not a valid project.' % cleaned_data['project']))
+
     class Meta:
         model = Slip
         fields = ('name', 'client', 'project')
+
