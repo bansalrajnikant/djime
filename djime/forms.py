dfd350cbe085962a0761594da1ed6a2c41b7c95c
Jakob Torp Svendsen <jakob@revealit.dk>
2009-02-09 16:10:38 +0100

    rearranged the code to trim it a bit.

--- djime/forms.py
+++ djime/forms.py
@@ -14,22 +14,18 @@ class SlipAddForm(forms.ModelForm):
         """
         # As this method is for the project field, we only get the clean_data for the project.
         cleaned_data = self.cleaned_data['project']
-        if cleaned_data:
-            # if user choses no project after choosing a client: set cleaned data to None
-            if cleaned_data == u'-----------':
-                cleaned_data = None
-            else:
-                project = Project.objects.filter(name__iexact=cleaned_data).filter(members=self.data['user'])
-                if project:
-                    cleaned_data['project'] = project[0]
-                else:
-                    raise forms.ValidationError(_('%s is not a valid project.' % cleaned_data))
+        # if user choses no project after choosing a client or
         # if cleaned_data is an empty string, user has not entered a project: set cleaned data to None
-        else:
+        if cleaned_data == u'-----------' or cleaned_data == u'':
             cleaned_data = None
+        if cleaned_data:
+            project = Project.objects.filter(name__iexact=cleaned_data).filter(members=self.data['user'])
+            if project:
+                cleaned_data = project[0]
+            else:
+                raise forms.ValidationError(_('%s is not a valid project.' % cleaned_data))
         return cleaned_data
 
     class Meta:
         model = Slip
         fields = ('name', 'client', 'project')
-
