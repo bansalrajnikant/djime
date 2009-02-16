a6c55e4bc85c4aaa0f3b2e38f4ecd4d2c9c0b6d4
Jakob Torp Svendsen <jakob@revealit.dk>
2009-02-16 10:42:19 +0100

    Bug fix, setting user for timeslice based on slip.

--- djime/signals.py
+++ djime/signals.py
@@ -3,6 +3,7 @@ def timeslice_save(sender, **kwargs):
     time_slice = kwargs['instance']
     time_slice.week_number = time_slice.begin.isocalendar()[1]
     time_slice.slip.updated = datetime.datetime.now()
+    time_slice.user = time_slice.slip.user
     time_slice.slip.save()    
     if time_slice.end:
         time = time_slice.end - time_slice.begin
