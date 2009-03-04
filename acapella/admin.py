7a9b5f7d6e68d3ab51b8e380ded7578be141611e
Jakob Torp Svendsen <jakob@revealit.dk>
2009-03-04 17:01:22 +0100

    Updated the acapella installation

--- acapella/admin.py
+++ acapella/admin.py
@@ -2,16 +2,16 @@ from django.contrib import admin
 from acapella.models import PermissionSlip, PermissionProject, PermissionClient
 
 class PermissionSlipAdmin(admin.ModelAdmin):
-    list_display = ('slip', 'user', 'permission')
-    ordering = ('-slip',)
+    list_display = ('model', 'user', 'permission')
+    ordering = ('-model',)
 
 class PermissionProjectAdmin(admin.ModelAdmin):
-    list_display = ('project', 'user', 'permission')
-    ordering = ('-project',)
+    list_display = ('model', 'user', 'permission')
+    ordering = ('-model',)
 
 class PermissionClientAdmin(admin.ModelAdmin):
-    list_display = ('client', 'user', 'permission')
-    ordering = ('-client',)
+    list_display = ('model', 'user', 'permission')
+    ordering = ('-model',)
 
 admin.site.register(PermissionSlip, PermissionSlipAdmin)
 admin.site.register(PermissionProject, PermissionProjectAdmin)
