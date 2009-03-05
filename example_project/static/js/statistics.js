222116bddf5b17063d7a6221db92391d53adeaf6
Jakob Torp Svendsen <jakob@revealit.dk>
2009-03-05 15:55:40 +0100

    updated datepicker and implemented daterangepicker for jQuery ui 1.6

--- example_project/static/js/statistics.js
+++ example_project/static/js/statistics.js
@@ -1,14 +1,23 @@
 $(document).ready(function () {
-  $('#id_date').datepicker({
-    dateFormat: 'yy-mm-dd',
-    rangeSelect: true,
-    firstDay: 1,
-    numberOfMonths: 2,
-    maxDate: '1d'
-  });
 	$('#billing-date').datepicker({
     dateFormat: 'yy-mm-dd',
     firstDay: 1,
-    maxDate: '1d'
+    maxDate: '1d',
+		changeMonth: true,
+		changeYear: true,
+	});
+	$('#id_date').daterangepicker({
+    firstDay: 1,
+		dateFormat: 'yy-mm-dd',
+		doneButtonText: 'Choose',
+		presets: {dateRange: 'Custom daterange'},
+		presetRanges: [
+			{text: 'Last 7 days', dateStart: 'today-7days', dateEnd: 'today' },
+			{text: 'Month to date', dateStart: function(){ return Date.parse('today').moveToFirstDayOfMonth();  }, dateEnd: 'today' },
+		],
+		earliestDate: Date.parse('-2years'),
+		latestDate: Date.parse('+0years'),
+		changeMonth: true,
+		changeYear: true,
 	});
 });
